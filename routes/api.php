4da882d233196c63fdb54df1703c3e77c6968340
freelancernishad <freelancernishad123@gmail.com>
2024-03-18 10:42:53 +0600

    update

--- routes/api.php
+++ routes/api.php
@@ -71,6 +71,43 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::get('/user-access', function (Request $request) {
         return 'user access';
     });
+
+
+
+
+
+
+
+
+    // Article routes
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/by-category/{categoryId}', [ArticleController::class, 'getByCategory']);
+Route::post('/articles', [ArticleController::class, 'store']);
+Route::get('/articles/{id}', [ArticleController::class, 'show']);
+Route::post('/articles/{id}', [ArticleController::class, 'update']);
+Route::delete('/articles/{id}', [ArticleController::class, 'destroy']);
+
+// Comment routes
+Route::get('/comments', [CommentController::class, 'index']);
+Route::post('/comments', [CommentController::class, 'store']);
+Route::get('/comments/{id}', [CommentController::class, 'show']);
+Route::post('/comments/{id}', [CommentController::class, 'update']);
+Route::delete('/comments/{id}', [CommentController::class, 'destroy']);
+
+// Category routes
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
+Route::post('/categories/{id}', [CategoryController::class, 'update']);
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+Route::get('/categories/{id}/subcategories', [CategoryController::class, 'getSubcategories']);
+
+
+
+
+
+
+
 });
 
 
@@ -136,25 +173,3 @@ Route::middleware('auth:student')->group(function () {
 
 
 
-// Article routes
-Route::get('/articles', [ArticleController::class, 'index']);
-Route::get('/articles/by-category/{categoryId}', [ArticleController::class, 'getByCategory']);
-Route::post('/articles', [ArticleController::class, 'store']);
-Route::get('/articles/{id}', [ArticleController::class, 'show']);
-Route::post('/articles/{id}', [ArticleController::class, 'update']);
-Route::delete('/articles/{id}', [ArticleController::class, 'destroy']);
-
-// Comment routes
-Route::get('/comments', [CommentController::class, 'index']);
-Route::post('/comments', [CommentController::class, 'store']);
-Route::get('/comments/{id}', [CommentController::class, 'show']);
-Route::post('/comments/{id}', [CommentController::class, 'update']);
-Route::delete('/comments/{id}', [CommentController::class, 'destroy']);
-
-// Category routes
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::post('/categories', [CategoryController::class, 'store']);
-Route::get('/categories/{id}', [CategoryController::class, 'show']);
-Route::post('/categories/{id}', [CategoryController::class, 'update']);
-Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
-Route::get('/categories/{id}/subcategories', [CategoryController::class, 'getSubcategories']);
